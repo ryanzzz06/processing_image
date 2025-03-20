78f6f9d732e3884066b6a1e244a197c10c8499a8
ryanzzz06 <ryan.bou-jaoude@efrei.net>
2025-03-20 18:26:12 +0100

    first commit

---
+++ bmp8.c
@@ -0,0 +1,5 @@
+//
+// Created by Ryan Bou Jaoude on 20/03/2025.
+//
+
+#include "bmp8.h"
---
+++ bmp8.h
@@ -0,0 +1,8 @@
+//
+// Created by Ryan Bou Jaoude on 20/03/2025.
+//
+
+#ifndef BMP8_H
+#define BMP8_H
+
+#endif //BMP8_H
